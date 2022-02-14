944c014aa5984373a42b1604ff2e13478f7a11f7
mauricioACV <mauricio.campusano@gmail.com>
2022-02-14 13:34:46 -0300

    Implementación de ejemplo React Router v6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import NavbarPage from "./layouts/NavbarPage";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<NavbarPage />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<Navigate replace to="/" />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/layouts/NavbarPage.jsx
@@ -0,0 +1,25 @@
+import { Outlet, Link } from "react-router-dom";
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+export default function NavbarPage() {
+  return (
+    <>
+      <Navbar className="navBg" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand as={Link} to="/" >React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/" >Home</Nav.Link>
+              <Nav.Link as={Link} to="/about" >About</Nav.Link>
+              <Nav.Link as={Link} to="/contact" >Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <section>
+        <Outlet></Outlet>
+      </section>
+    </>
+  );
+}
